1b41f5882e06531e8952dfe8763a018e70d38922
wmik <williemik.wmik@gmail.com>
2020-09-04 10:27:31 +0300

    feat: vertical & horizontal stack components - Implement vertical & horizontal stack component - Add dependencies - Update documentation - Setup example usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
 import React from 'react';
-import './styles.css';
+import styled from 'styled-components';
+
+function px(value) {
+  return typeof value === 'number' ? `${value}px` : value;
+}
+
+function getSpacing(position) {
+  return (props) => {
+    switch (position) {
+      case 'top':
+        return px(props.gapTop || props.gap);
+      case 'left':
+        return px(props.gapLeft || props.gap);
+      case 'right':
+        return px(props.gapRight || props.gap);
+      case 'bottom':
+        return px(props.gapBottom || props.gap);
+      default:
+        return '';
+    }
+  };
+}
+
+const HStack = styled.div`
+  display: flex;
+  > * {
+    margin: ${(props) => px(props.gap)};
+    margin-top: ${getSpacing('top')};
+    margin-right: ${getSpacing('right')};
+    margin-bottom: ${getSpacing('bottom')};
+    margin-left: ${getSpacing('left')};
+  }
+`;
+
+const VStack = styled(HStack)`
+  flex-direction: column;
+`;
+
+const Square = styled.div`
+  background-color: crimson;
+  width: 24px;
+  height: 24px;
+`;
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <article>
+      <h2>Horizontal stack</h2>
+      <HStack gapRight={40} gap={4}>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </HStack>
+      <h2>Vertical stack</h2>
+      <VStack gapTop={4}>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </VStack>
+    </article>
   );
 }
